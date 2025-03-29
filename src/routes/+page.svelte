c7a14b4edef67ed3cf19cf9676db82b687fb50cd
Jonathan St-Onge <jonathanstonge7@gmail.com>
2025-03-28 20:24:47 -0400

    bit by bit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,18 @@
 <script>
 	import { setContext } from "svelte";
 	// import { browser } from "$app/environment";
-	// import Index from "$components/Index.svelte";
-	// import copy from "$data/copy.json";
+	import Index from "$components/Index.svelte";
+	import copy from "$data/copy.json";
 	// import version from "$utils/version.js";
 
-	// let { data } = $props();
+	let { data } = $props();
 
 	
 	// const { title, description, url, keywords } = copy;
-	// setContext("copy", copy);
-	// setContext("data", data);
+	setContext("copy", copy);
+	setContext("data", data);
 </script>
 
-<h1>hello</h1>
 
-<!-- <Index /> -->
+
+<Index />
